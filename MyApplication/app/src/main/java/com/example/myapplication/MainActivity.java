7912ca03b21b284c78f52a04e33e81ddc8f4279a
comye1 <50735594+comye1@users.noreply.github.com>
2020-03-31 14:03:27 +0900

    위젯 객체 추가 widget objects added

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button button1, button2;
+    RadioGroup radioGroup;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = (EditText)findViewById(R.id.editText);
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        imageView = (ImageView) findViewById(R.id.imageView);
     }
 }
