6299b763fef7d34ca0beaa14f56ddc0f2081b727
comye1 <50735594+comye1@users.noreply.github.com>
2020-03-31 14:17:11 +0900

    radioGroup eventListner added

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,5 +28,20 @@ public class MainActivity extends AppCompatActivity {
         button2 = (Button) findViewById(R.id.button2);
         radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
         imageView = (ImageView) findViewById(R.id.imageView);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch(checkedId){
+                    case R.id.radioButton1:
+                        imageView.setImageResource(R.drawable.oreo_logo);
+                        break;
+                    case R.id.radioButton2:
+                        imageView.setImageResource(R.drawable.pie_logo);
+                        break;
+                }
+            }
+        });
     }
+
 }
