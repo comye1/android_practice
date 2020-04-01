e7c23248d9ba9d775a1dcbe2a14fceef1cedc8e9
comye1 <50735594+comye1@users.noreply.github.com>
2020-04-01 10:57:41 +0900

    TextView id added and declared

--- FavoritePet/app/src/main/java/com/example/favoritepet/MainActivity.java
+++ FavoritePet/app/src/main/java/com/example/favoritepet/MainActivity.java
@@ -9,10 +9,12 @@ import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     CheckBox checkStart;
+    TextView invisibleText;
     RadioGroup radioGroup;
     RadioButton dog, cat, rabbit;
     Button selectButton;
@@ -24,12 +26,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         checkStart = (CheckBox)findViewById(R.id.checkStart);
+        invisibleText = (TextView)findViewById(R.id.invisibleText);
         radioGroup = (RadioGroup)findViewById(R.id.radioGroup);
         dog = (RadioButton)findViewById(R.id.radioDog);
         cat = (RadioButton)findViewById(R.id.radioCat);
         rabbit = (RadioButton)findViewById(R.id.radioRabbit);
         selectButton = (Button)findViewById(R.id.buttonSelected);
         imageView = (ImageView)findViewById(R.id.imageView);
- 
+
+        checkStart.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(checkStart.isChecked()) {
+
+                }
+            }
+        });
     }
 }
