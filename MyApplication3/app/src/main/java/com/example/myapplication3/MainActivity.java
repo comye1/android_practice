72ed544fd07b3bfaad675a9bb6efd9431a64cba1
comye1 <50735594+comye1@users.noreply.github.com>
2020-04-04 18:30:37 +0900

    editText eventListenr (SetOnKeyLIstener)

--- MyApplication3/app/src/main/java/com/example/myapplication3/MainActivity.java
+++ MyApplication3/app/src/main/java/com/example/myapplication3/MainActivity.java
@@ -3,8 +3,14 @@ package com.example.myapplication3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +19,17 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.mipmap.ic_launcher);
+
+        editText = (EditText) findViewById(R.id.editText);
+
+        editText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+
+                Toast.makeText(getApplicationContext(), editText.getText().toString(), Toast.LENGTH_SHORT).show();
+
+                return false;
+            }
+        });
     }
 }
