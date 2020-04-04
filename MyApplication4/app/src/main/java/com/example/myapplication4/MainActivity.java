a3d91916e319f82d85a0226f676113b543533269
comye1 <50735594+comye1@users.noreply.github.com>
2020-04-04 21:27:18 +0900

    Button EventListener -> Rotationing function implemented

--- MyApplication4/app/src/main/java/com/example/myapplication4/MainActivity.java
+++ MyApplication4/app/src/main/java/com/example/myapplication4/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.myapplication4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button)findViewById(R.id.button);
+        imageView = (ImageView)findViewById(R.id.imageView2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.setRotation(imageView.getRotation()-10);
+            }
+        });
+
     }
 }
