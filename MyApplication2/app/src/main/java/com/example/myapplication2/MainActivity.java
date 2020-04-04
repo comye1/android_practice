8f33f15683267d911247c12c44bb786a21f993b0
comye1 <50735594+comye1@users.noreply.github.com>
2020-04-04 17:10:08 +0900

    enabling checkbox eventListener => setEnabled

--- MyApplication2/app/src/main/java/com/example/myapplication2/MainActivity.java
+++ MyApplication2/app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -3,8 +3,14 @@ package com.example.myapplication2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    CheckBox cbEnabled, cbClickable, cbRotate;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +19,21 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.mipmap.ic_launcher);
+
+        button = (Button)findViewById(R.id.button);
+        cbEnabled = (CheckBox)findViewById(R.id.cbEnabled);
+        cbClickable = (CheckBox)findViewById(R.id.cbClickable);
+        cbRotate = (CheckBox)findViewById(R.id.cbRotate);
+
+        cbEnabled.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    button.setEnabled(false);
+                }else{
+                    button.setEnabled(true);
+                }
+            }
+        });
     }
 }
