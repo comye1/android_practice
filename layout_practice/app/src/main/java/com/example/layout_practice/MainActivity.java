95dfd1962ab781f3f9751b0b7a5f46644f4760a7
comye1 <50735594+comye1@users.noreply.github.com>
2020-04-06 18:37:19 +0900

    Java 코드로 화면 만들기 - LinearLayout, Button (예제 5-9)

--- layout_practice/app/src/main/java/com/example/layout_practice/MainActivity.java
+++ layout_practice/app/src/main/java/com/example/layout_practice/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,5 +24,17 @@ public class MainActivity extends AppCompatActivity {
         baseLayout.setOrientation(LinearLayout.VERTICAL);
         baseLayout.setBackgroundColor(Color.rgb(0,255,0));
         setContentView(baseLayout, params);
+
+        Button btn = new Button(this);
+        btn.setText("버튼입니다.");
+        btn.setBackgroundColor(Color.MAGENTA);
+        baseLayout.addView(btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "코드로 생성한 버튼입니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
