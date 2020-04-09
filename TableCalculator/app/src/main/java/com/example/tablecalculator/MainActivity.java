42cda4ed071081844fc135706c695528a7e244f8
comye1 <50735594+comye1@users.noreply.github.com>
2020-04-10 01:08:01 +0900

    number Buttons implemented

--- TableCalculator/app/src/main/java/com/example/tablecalculator/MainActivity.java
+++ TableCalculator/app/src/main/java/com/example/tablecalculator/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
             R.id.btnNum4, R.id.btnNum5, R.id.btnNum6, R.id.btnNum7,
             R.id.btnNum8, R.id.btnNum9};
     int i;
+    String num1, num2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,19 +28,44 @@ public class MainActivity extends AppCompatActivity {
 
         setTitle("테이블레이아웃 계산기");
 
-        edit1 = (EditText)findViewById(R.id.editNum1);
-        edit2 = (EditText)findViewById(R.id.editNum2);
-        btnAdd = (Button)findViewById(R.id.btnAdd);
-        btnSub = (Button)findViewById(R.id.btnSub);
-        btnMul = (Button)findViewById(R.id.btnMul);
-        btnDiv = (Button)findViewById(R.id.btnDiv);
-        textResult = (TextView)findViewById(R.id.textResult);
+        edit1 = (EditText) findViewById(R.id.editNum1);
+        edit2 = (EditText) findViewById(R.id.editNum2);
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnSub = (Button) findViewById(R.id.btnSub);
+        btnMul = (Button) findViewById(R.id.btnMul);
+        btnDiv = (Button) findViewById(R.id.btnDiv);
+        textResult = (TextView) findViewById(R.id.textResult);
+
+        for (i = 0; i < numButtons.length; i++) {
+            numButtons[i] = (Button)findViewById(numBtnIDs[i]);
+        }
+
+        for(i=0; i<numButtons.length; i++){
+            final int index;
+            index = i;
+
+            numButtons[index].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(edit1.isFocused()){
+                        num1 = edit1.getText().toString() + numButtons[index].getText().toString();
+                        edit1.setText(num1);
+                    }else if(edit2.isFocused()){
+                        num2 = edit2.getText().toString() + numButtons[index].getText().toString();
+                        edit2.setText(num2);
+                    }else{
+                        Toast.makeText(getApplicationContext(),
+                                "먼저 에디트텍스트를 클릭하세요.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
 
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String num1 = edit1.getText().toString();
-                String num2 = edit2.getText().toString();
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
                 int result = Integer.parseInt(num1) + Integer.parseInt(num2);
 
                 textResult.setText("계산 결과 : " + result);
@@ -49,8 +75,8 @@ public class MainActivity extends AppCompatActivity {
         btnSub.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String num1 = edit1.getText().toString();
-                String num2 = edit2.getText().toString();
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
                 int result = Integer.parseInt(num1) - Integer.parseInt(num2);
 
                 textResult.setText("계산 결과 : " + result);
@@ -60,8 +86,8 @@ public class MainActivity extends AppCompatActivity {
         btnMul.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String num1 = edit1.getText().toString();
-                String num2 = edit2.getText().toString();
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
                 int result = Integer.parseInt(num1) * Integer.parseInt(num2);
 
                 textResult.setText("계산 결과 : " + result);
@@ -72,8 +98,8 @@ public class MainActivity extends AppCompatActivity {
         btnDiv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String num1 = edit1.getText().toString();
-                String num2 = edit2.getText().toString();
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
                 int result = Integer.parseInt(num1) / Integer.parseInt(num2);
 
                 textResult.setText("계산 결과 : " + result);
