1946acc417eb4276ff9d7a18c8325585c2470bf4
comye1 <50735594+comye1@users.noreply.github.com>
2020-04-14 16:52:29 +0900

    MainActivity completed

--- TimeReservation/app/src/main/java/com/example/timereservation/MainActivity.java
+++ TimeReservation/app/src/main/java/com/example/timereservation/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.timereservation;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.Chronometer;
@@ -19,10 +23,74 @@ public class MainActivity extends AppCompatActivity {
     CalendarView calendarView;
     TimePicker timePicker;
     TextView textView;
-    
+    String string_date, string_time;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        chronometer = (Chronometer)findViewById(R.id.chronometer);
+        button_start = (Button)findViewById(R.id.button_start);
+        button_end = (Button)findViewById(R.id.button_end);
+        radioGroup = (RadioGroup)findViewById(R.id.radioGroup);
+        radioButton_cal = (RadioButton)findViewById(R.id.radioButton_cal);
+        radioButton_time = (RadioButton)findViewById(R.id.radioButton_time);
+        calendarView = (CalendarView)findViewById(R.id.calendarView);
+        timePicker = (TimePicker)findViewById(R.id.timePicker);
+        textView = (TextView)findViewById(R.id.textView);
+
+        radioGroup.setVisibility(View.INVISIBLE);
+        timePicker.setVisibility(View.INVISIBLE);
+        calendarView.setVisibility(View.INVISIBLE);
+
+        button_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                radioGroup.setVisibility(View.VISIBLE);
+                chronometer.setBase(SystemClock.elapsedRealtime());
+                chronometer.start();
+                chronometer.setTextColor(Color.RED);
+            }
+        });
+
+        radioButton_time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calendarView.setVisibility(View.INVISIBLE);
+                timePicker.setVisibility(View.VISIBLE);
+            }
+        });
+
+        radioButton_cal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timePicker.setVisibility(View.INVISIBLE);
+                calendarView.setVisibility(View.VISIBLE);
+            }
+        });
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                string_date = year + "년" + (month + 1) + "월" + dayOfMonth + "일";
+            }
+        });
+
+        timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+                string_time = hourOfDay + "시" + minute + "분 예약됨";
+            }
+        });
+
+        button_end.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chronometer.stop();
+                chronometer.setTextColor(Color.BLUE);
+                textView.setText(string_date+string_time);
+            }
+        });
     }
 }
