4f06edf32d66559c182ace7103cc51b005357be1
comye1 <50735594+comye1@users.noreply.github.com>
2020-04-14 16:03:55 +0900

    layout implemented

--- TimeReservation/app/src/main/java/com/example/timereservation/MainActivity.java
+++ TimeReservation/app/src/main/java/com/example/timereservation/MainActivity.java
@@ -3,9 +3,23 @@ package com.example.timereservation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.Chronometer;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
-
+    Chronometer chronometer;
+    Button button_start, button_end;
+    RadioGroup radioGroup;
+    RadioButton radioButton_cal, radioButton_time;
+    CalendarView calendarView;
+    TimePicker timePicker;
+    TextView textView;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
