eeae3bd82d54f291674b2307fe332c9c903e13c1
comye1 <50735594+comye1@users.noreply.github.com>
2020-04-14 17:30:25 +0900

    AutoCompleteTextView / MultiAutoCompleteTextView / ArrayAdapter / CommaTokenizer

--- AdvancedWidgets/app/src/main/java/com/example/advancedwidgets/MainActivity.java
+++ AdvancedWidgets/app/src/main/java/com/example/advancedwidgets/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.advancedwidgets;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.MultiAutoCompleteTextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] items = {"CSI-NY", "CSI-LV", "CSI-MI", "Friends", "Fringe", "Lost"};
+
+        AutoCompleteTextView auto = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, items);
+        auto.setAdapter(adapter);
+
+        MultiAutoCompleteTextView multi = (MultiAutoCompleteTextView) findViewById(R.id.multiAutoCompleteTextView);
+        MultiAutoCompleteTextView.CommaTokenizer commaTokenizer = new MultiAutoCompleteTextView.CommaTokenizer();
+        multi.setTokenizer(commaTokenizer);
+        multi.setAdapter(adapter);
     }
+
 }
