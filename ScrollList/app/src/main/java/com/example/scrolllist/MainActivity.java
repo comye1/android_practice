8600196156536081340116947c375a23e2bd4493
comye1 <50735594+comye1@users.noreply.github.com>
2020-04-15 13:38:53 +0900

    ListView // ArrayList 추가 기능

--- ScrollList/app/src/main/java/com/example/scrolllist/MainActivity.java
+++ ScrollList/app/src/main/java/com/example/scrolllist/MainActivity.java
@@ -1,13 +1,47 @@
 package com.example.scrolllist;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button button;
+    ListView listView;
+    String newStr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = (EditText)findViewById(R.id.editText);
+        button = (Button) findViewById(R.id.button);
+        listView = (ListView) findViewById(R.id.listView);
+
+        final ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("010-0000-0000");
+        arrayList.add("010-0000-1111");
+
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item,arrayList);
+        listView.setAdapter(adapter);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                newStr = editText.getText().toString();
+                arrayList.add(newStr);
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
 }
