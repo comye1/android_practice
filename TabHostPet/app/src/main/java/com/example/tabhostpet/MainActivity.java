2b934e1f7d1fdd948b4ae90b1be3bcffd947e3db
comye1 <50735594+comye1@users.noreply.github.com>
2020-04-15 19:38:57 +0900

    TabHost

--- TabHostPet/app/src/main/java/com/example/tabhostpet/MainActivity.java
+++ TabHostPet/app/src/main/java/com/example/tabhostpet/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.tabhostpet;
-
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.app.ActionBar;
+import android.app.TabActivity;
+import android.os.Bundle;
+import android.widget.TabHost;
+import android.widget.TabWidget;
+
+import com.example.tabhostpet.R;
+
+@SuppressWarnings("deprecation")
+
+public class MainActivity extends TabActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabHost tabHost = getTabHost();
+        tabHost.setup();
+
+        TabHost.TabSpec tsDog = tabHost.newTabSpec("DOG").setIndicator("강아지");
+        tsDog.setContent(R.id.dog);
+        tabHost.addTab(tsDog);
+
+        TabHost.TabSpec tsCat= tabHost.newTabSpec("CAT").setIndicator("고양이");
+        tsCat.setContent(R.id.cat);
+        tabHost.addTab(tsCat);
+
+        TabHost.TabSpec tsRabbit = tabHost.newTabSpec("RABBIT").setIndicator("토끼");
+        tsRabbit.setContent(R.id.rabbit);
+        tabHost.addTab(tsRabbit);
+
+        TabHost.TabSpec tsHorse = tabHost.newTabSpec("HORSE").setIndicator("말");
+        tsHorse.setContent(R.id.horse);
+        tabHost.addTab(tsHorse);
+
+        tabHost.setCurrentTab(0);
+
     }
 }
