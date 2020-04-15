a336f4c70722a0a3d87200c4b957b6b721c0b722
comye1 <50735594+comye1@users.noreply.github.com>
2020-04-15 16:59:14 +0900

    ViewFlipper --> start/stopFlipping, setFlipInterval

--- AutoFlipping/app/src/main/java/com/example/autoflipping/MainActivity.java
+++ AutoFlipping/app/src/main/java/com/example/autoflipping/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.autoflipping;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button_start, button_stop;
+        final ViewFlipper viewFlipper;
+
+        button_start = (Button)findViewById(R.id.button_start);
+        button_stop = (Button)findViewById(R.id.button_stop);
+        viewFlipper = (ViewFlipper)findViewById(R.id.viewFlipper);
+        viewFlipper.setFlipInterval(1000);
+
+        button_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewFlipper.startFlipping();
+            }
+        });
+
+        button_stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewFlipper.stopFlipping();
+            }
+        });
     }
 }
