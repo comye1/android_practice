ecdd5bbab5412b54869038df7e81a2ac9fa8ed07
comye1 <50735594+comye1@users.noreply.github.com>
2020-04-15 14:48:00 +0900

    ScrollView > Buttons

--- AdvancedWidgets/app/src/main/java/com/example/advancedwidgets/MainActivity.java
+++ AdvancedWidgets/app/src/main/java/com/example/advancedwidgets/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.advancedwidgets;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.MultiAutoCompleteTextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,17 +15,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        String[] items = {"CSI-NY", "CSI-LV", "CSI-MI", "Friends", "Fringe", "Lost"};
-
-        AutoCompleteTextView auto = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, items);
-        auto.setAdapter(adapter);
-
-        MultiAutoCompleteTextView multi = (MultiAutoCompleteTextView) findViewById(R.id.multiAutoCompleteTextView);
-        MultiAutoCompleteTextView.CommaTokenizer commaTokenizer = new MultiAutoCompleteTextView.CommaTokenizer();
-        multi.setTokenizer(commaTokenizer);
-        multi.setAdapter(adapter);
     }
 
 }
