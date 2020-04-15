a5e7025b35cc5b1743cdf03143d9aba31f25f2b0
comye1 <50735594+comye1@users.noreply.github.com>
2020-04-15 16:27:52 +0900

    ViewFlipper

--- AdvancedWidgets/app/src/main/java/com/example/advancedwidgets/MainActivity.java
+++ AdvancedWidgets/app/src/main/java/com/example/advancedwidgets/MainActivity.java
@@ -8,13 +8,37 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.MultiAutoCompleteTextView;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button prev, next;
+        final ViewFlipper viewFlipper;
+
+        prev = (Button)findViewById(R.id.prev);
+        next = (Button)findViewById(R.id.next);
+        viewFlipper = (ViewFlipper)findViewById(R.id.viewFlipper);
+
+        prev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewFlipper.showPrevious();
+            }
+        });
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewFlipper.showNext();
+            }
+        });
     }
 
 }
