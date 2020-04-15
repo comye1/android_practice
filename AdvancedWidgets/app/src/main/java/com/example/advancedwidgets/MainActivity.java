2dc1ec208b15bebef7e95e60343d23a8a33ff579
comye1 <50735594+comye1@users.noreply.github.com>
2020-04-15 17:50:15 +0900

    TabHost, TabSpec // 실행안됨

--- AdvancedWidgets/app/src/main/java/com/example/advancedwidgets/MainActivity.java
+++ AdvancedWidgets/app/src/main/java/com/example/advancedwidgets/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.advancedwidgets;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.TabActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -8,37 +10,34 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.MultiAutoCompleteTextView;
+import android.widget.TabHost;
 import android.widget.ViewFlipper;
 
-public class MainActivity extends AppCompatActivity {
+@SuppressWarnings("deprecation")
+public class MainActivity extends TabActivity{
 
-
+    TabHost tabHost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button prev, next;
-        final ViewFlipper viewFlipper;
+        tabHost = (TabHost)findViewById(R.id.tabHost);
 
-        prev = (Button)findViewById(R.id.prev);
-        next = (Button)findViewById(R.id.next);
-        viewFlipper = (ViewFlipper)findViewById(R.id.viewFlipper);
+        TabHost.TabSpec tabSpecSong = tabHost.newTabSpec("SONG").setIndicator("음악별");
+        tabSpecSong.setContent(R.id.tabSong);
+        tabHost.addTab(tabSpecSong);
 
-        prev.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                viewFlipper.showPrevious();
-            }
-        });
+        TabHost.TabSpec tabSpecArtist = tabHost.newTabSpec("ARTIST").setIndicator("가수별");
+        tabSpecArtist.setContent(R.id.tabArtist);
+        tabHost.addTab(tabSpecArtist);
 
-        next.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                viewFlipper.showNext();
-            }
-        });
+        TabHost.TabSpec tabSpecAlbum = tabHost.newTabSpec("ALBUM").setIndicator("앨범별");
+        tabSpecAlbum.setContent(R.id.tabAlbum);
+        tabHost.addTab(tabSpecAlbum);
+
+        tabHost.setCurrentTab(0);
     }
 
 }
