09ab8f38558eccf5f05edfbb983bc1b58b89c404
comye1 <50735594+comye1@users.noreply.github.com>
2020-04-29 15:07:39 +0900

    Layout & Variables

--- SimpleDiary/app/src/main/java/com/example/simplediary/MainActivity.java
+++ SimpleDiary/app/src/main/java/com/example/simplediary/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.simplediary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    DatePicker datePicker;
+    EditText editText_Diary;
+    Button button_Write;
+    String fileName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("일기장");
+
+        datePicker = (DatePicker) findViewById(R.id.datePicker);
+        editText_Diary = (EditText) findViewById(R.id.editText_Diary);
+        button_Write = (Button) findViewById(R.id.button_Write);
+
     }
 }
