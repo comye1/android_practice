128e6df127fdbcf22b878941f9588a0e37bc1dfe
comye1 <50735594+comye1@users.noreply.github.com>
2020-05-28 15:32:02 +0900

    db 테스트 - 쓰기

--- FireBaseDBTest/app/src/main/java/com/example/firebasedbtest/AuthActivity.java
+++ FireBaseDBTest/app/src/main/java/com/example/firebasedbtest/AuthActivity.java
@@ -6,8 +6,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -24,12 +26,19 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class AuthActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private FirebaseUser user;
-    Button btn_Logout, btn_Revoke;
+    private EditText editText;
+    private Button btn_Logout, btn_Revoke, btn_Submit;
+    private DatabaseReference mDatabase;// ...
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +48,32 @@ public class AuthActivity extends AppCompatActivity {
         user = mAuth.getCurrentUser();
 
         Toast.makeText(getApplicationContext(), user.getEmail() + "로 로그인 되었습니다.", Toast.LENGTH_LONG).show();
+//        Toast.makeText(getApplicationContext(), user.getUid(), Toast.LENGTH_LONG).show();
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+//        mDatabase.child("User").child(user.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                User user = dataSnapshot.getChildren()
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        });
+        editText = findViewById(R.id.editText);
+
+        btn_Submit = findViewById(R.id.btn_Submit);
+
+        btn_Submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = editText.getText().toString();
+                mDatabase.child("User").child(mAuth.getUid()).child("text").setValue(text);
+                //User 밑 Uid 밑 text로 넣음
+            }
+        });
 
         btn_Logout = findViewById(R.id.btn_LogOut);
         btn_Revoke = findViewById(R.id.btn_Revoke);
